799a48d03de6fe7c1e2d434f52b4dbe25e64f767
윤민석 <ykmvm147@naver.com>
2022-05-09 21:11:41 +0900

    Change test code and lib

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @Method: Returns the Plural form of any noun.
+ * @Param {string}
+ * @Return {string}
+ */
+export declare function getPlural(str: any): string;
+/**
+ * @Method: Returns the Singular form of any noun.
+ * @Param {string}
+ * @Return {string}
+ */
+export declare function getSingular(str: string): string;
