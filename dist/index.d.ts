378e5d5a4e6780482fb8a35099777c3ae4c7e240
윤민석 <ykmvm147@naver.com>
2022-05-10 20:58:11 +0900

    Deploy error....

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,2 @@
-/**
- * @Method: Returns the Plural form of any noun.
- * @Param {string}
- * @Return {string}
- */
 export declare function getPlural(str: any): string;
-/**
- * @Method: Returns the Singular form of any noun.
- * @Param {string}
- * @Return {string}
- */
 export declare function getSingular(str: string): string;
--- dist/index.js
+++ dist/index.js
@@ -2,20 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getSingular = exports.getPlural = void 0;
 var pluralize = require("pluralize");
-/**
- * @Method: Returns the Plural form of any noun.
- * @Param {string}
- * @Return {string}
- */
 function getPlural(str) {
     return pluralize.plural(str);
 }
 exports.getPlural = getPlural;
-/**
- * @Method: Returns the Singular form of any noun.
- * @Param {string}
- * @Return {string}
- */
 function getSingular(str) {
     return pluralize.singular(str);
 }
