6baffce76fafe4347b9f11ca942de88c8c3bd816
윤민석 <ykmvm147@naver.com>
2022-05-14 14:36:06 +0900

    Update darkmode css

--- lib/darkmode.ts
+++ lib/darkmode.ts
@@ -2,13 +2,13 @@ type optionsType = {
   bottom: string;
   right: string;
   left: string;
-  time: string;
-  mixColor: string;
+  transitionTime: string;
   backgroundColor: string;
-  buttonColorDark: string;
-  buttonColorLight: string;
+  buttonDarkColor: string;
+  buttonLightColor: string;
   content: string;
   saveInCookies: boolean;
+  animation: string;
 };
 
 export default class DarkMode {
@@ -17,19 +17,51 @@ export default class DarkMode {
       bottom: "30px",
       right: "30px",
       left: "unset",
-      time: "0.3s",
-      mixColor: "#fff",
+      transitionTime: "0.3s",
       backgroundColor: "#fff",
-      buttonColorDark: "#141414",
-      buttonColorLight: "#fff",
+      buttonDarkColor: "#141414",
+      buttonLightColor: "#fff",
       content: "",
       saveInCookies: true,
+      animation: "scale(100)",
     };
 
     options = { ...defaultOptions, ...options };
 
     const css = `
-      
+      .darkmode-layer {
+        background: ${options.backgroundColor};
+        transition: all ${options.transitionTime} ease;
+      }
+      .darkmode-layer--simple {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        transform: ${options.animation};
+      }
+      .darkmode-toggle {
+        background: ${options.buttonDarkColor};
+        width: 3rem;
+        height: 3rem;
+        position: fixed;
+        border-radius: 50%;
+        border:none;
+        right: ${options.right};
+        bottom: ${options.bottom};
+        left: ${options.left};
+        cursor: pointer;
+        transition: all 0.5s ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .darkmode-toggle--white {
+        background: ${options.buttonLightColor};
+      }
     `;
+
+    const layer = document.createElement("div");
+    const button = document.createElement("button");
   }
 }
