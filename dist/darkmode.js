1b14ea209752f1821a274521acc48d151d5947ba
윤민석 <ykmvm147@naver.com>
2022-07-01 20:47:54 +0900

    [Feat] Update version

--- dist/darkmode.js
+++ dist/darkmode.js
@@ -38,7 +38,7 @@ var Darkmode = /** @class */ (function () {
                 }
                 button.classList.toggle("darkmode-button-whiteType");
                 layer.classList.toggle("darkmode-container");
-                document.body.classList.toggle("darkmode-activated");
+                // document.body.classList.toggle("darkmode-body-color");
                 window.localStorage.setItem("darkmode", (!isDarkmode).toString());
             });
         };
@@ -47,7 +47,7 @@ var Darkmode = /** @class */ (function () {
             var layer = _this.layer;
             var button = _this.button;
             layer.classList.toggle("darkmode-container");
-            document.body.classList.toggle("darkmode-activated");
+            // document.body.classList.toggle("darkmode-body-color");
             window.localStorage.setItem("darkmode", (!isDarkmode).toString());
             button.setAttribute("aria-label", "De-activate dark mode");
             button.setAttribute("aria-checked", "true");
@@ -66,15 +66,16 @@ var Darkmode = /** @class */ (function () {
             buttonHeight: "3rem",
         };
         options = __assign(__assign({}, defaultOptions), options);
-        var css = "\n      .darkmode-container{\n        position: fixed;\n        top: 0;\n        left: 0;\n        width: 100%;\n        height: 100%;\n        background : ".concat(options.backgroundColor, ";\n        pointer-events: none;\n        mix-blend-mode: difference;\n      }\n\n      .darkmode-button {\n        background: ").concat(options.buttonDarkColor, ";\n        width: ").concat(options.buttonWidth, ";\n        height: ").concat(options.buttonHeight, ";\n        position: fixed;\n        border-radius: 50%;\n        border:none;\n        right: ").concat(options.right, ";\n        bottom: ").concat(options.bottom, ";\n        left: ").concat(options.left, ";\n        cursor: pointer;\n        transition: all 0.5s ease;\n        display: flex;\n        justify-content: center;\n        align-items: center;\n      }\n\n      .darkmode-button-whiteType {\n        background: ").concat(options.buttonLightColor, ";\n      }\n\n      .darkmode-activated{\n        background: ").concat(options.backgroundColor, ";\n      }\n\n      img, .darkmode-ignore {\n        isolation: isolate;\n        display: inline-block;\n      }\n    ");
+        var css = "\n    .darkmode-layer{\n      position: fixed;\n      top: 0;\n      left: 0;\n      width: 100%;\n      height: 100%;\n      pointer-events: none;\n      mix-blend-mode: difference;\n      transition : all 0.5s ease;\n    }\n      .darkmode-container{\n        background : ".concat(options.backgroundColor, ";\n      }\n\n      .darkmode-button {\n        background: ").concat(options.buttonDarkColor, ";\n        width: ").concat(options.buttonWidth, ";\n        height: ").concat(options.buttonHeight, ";\n        position: fixed;\n        border-radius: 50%;\n        border:none;\n        right: ").concat(options.right, ";\n        bottom: ").concat(options.bottom, ";\n        left: ").concat(options.left, ";\n        cursor: pointer;\n        transition: all 0.5s ease;\n        display: flex;\n        justify-content: center;\n        align-items: center;\n      }\n\n      .darkmode-button-whiteType {\n        background: ").concat(options.buttonLightColor, ";\n      }\n\n      .darkmode-body-color{\n        background: ").concat(options.backgroundColor, ";\n      }\n\n      img, .darkmode-ignore {\n        isolation: isolate;\n        display: inline-block;\n      }\n    ");
         var layer = document.createElement("div");
         var button = document.createElement("button");
         button.innerHTML = options.content;
         var darkmodeActive = window.localStorage.getItem("darkmode") === "true";
+        layer.classList.add("darkmode-layer");
+        document.body.classList.add("darkmode-body-color");
         if (darkmodeActive === true) {
             button.classList.add("darkmode-button-whiteType");
             layer.classList.add("darkmode-container");
-            document.body.classList.add("darkmode-activated");
         }
         document.body.insertBefore(button, document.body.firstChild);
         document.body.insertBefore(layer, document.body.firstChild);
@@ -88,7 +89,7 @@ var Darkmode = /** @class */ (function () {
         this.time = options.transitionTime;
     }
     Darkmode.prototype.isActiveDark = function () {
-        return document.body.classList.contains("darkmode-activated");
+        return document.body.classList.contains("darkmode-container");
     };
     return Darkmode;
 }());
